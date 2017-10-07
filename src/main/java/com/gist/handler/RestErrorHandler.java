3c9dbe70ce3c8048cc1416f0077c6ad25c232067
vizsatiz <satis.vishnu@gmail.com>
2017-10-07 20:41:04 +0530

    DB Validation error handled as json

---
+++ src/main/java/com/gist/handler/RestErrorHandler.java
@@ -0,0 +1,67 @@
+package com.gist.handler;
+
+import com.gist.model.DBFieldValidationError;
+import com.gist.model.DBValidationError;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Created by kh1888 on 07/10/17.
+ */
+@ControllerAdvice
+public class RestErrorHandler {
+
+    private MessageSource messageSource;
+
+    @Autowired
+    public RestErrorHandler(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public DBValidationError processValidationError(MethodArgumentNotValidException ex) {
+        BindingResult result = ex.getBindingResult();
+        List<FieldError> fieldErrors = result.getFieldErrors();
+
+        return processFieldErrors(fieldErrors);
+    }
+
+    private DBValidationError processFieldErrors(List<FieldError> fieldErrors) {
+        DBValidationError dto = new DBValidationError();
+
+        for (FieldError fieldError: fieldErrors) {
+            String localizedErrorMessage = resolveLocalizedErrorMessage(fieldError);
+            dto.addFieldError(fieldError.getField(), localizedErrorMessage);
+        }
+
+        return dto;
+    }
+
+    private String resolveLocalizedErrorMessage(FieldError fieldError) {
+        Locale currentLocale =  LocaleContextHolder.getLocale();
+        String localizedErrorMessage = messageSource.getMessage(fieldError, currentLocale);
+
+        //If the message was not found, return the most accurate field error code instead.
+        //You can remove this check if you prefer to get the default error message.
+        if (localizedErrorMessage.equals(fieldError.getDefaultMessage())) {
+            String[] fieldErrorCodes = fieldError.getCodes();
+            localizedErrorMessage = fieldErrorCodes[0];
+        }
+
+        return localizedErrorMessage;
+    }
+}
